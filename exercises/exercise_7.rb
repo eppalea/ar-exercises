3af60b59e01ea3ca8c9ceb2b1650aa42fe10554a
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-30 17:42:17 +0000

    move the employee and store validations to their repective files in the lib folder

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,19 +10,6 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
-class Employee < ActiveRecord::Base
-  belongs_to :store
-  validates :first_name, :last_name, presence: true
-  validates_inclusion_of :hourly_rate, :in => 40..200
-  validates :store_id, presence: true
-end
-
-class Store < ActiveRecord::Base
-  has_many :employees
-  validates :name, length: { minimum: 3 }
-  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
-end
-
 puts "Please provide me with a store name."
 @reply = gets.chomp.to_s
 
