7cc65b05f3e29827f6b3294c6112eab5016197db
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-30 06:23:20 +0000

    add code inside Store and Employee model using ActiveRecord::Base. create employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,17 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Summer", last_name: "Acacia", hourly_rate: 40)
+@store1.employees.create(first_name: "Jack", last_name: "Hill", hourly_rate: 30)
+@store2.employees.create(first_name: "Jill", last_name: "Brown", hourly_rate: 35)
+@store2.employees.create(first_name: "Ryan", last_name: "Lyons", hourly_rate: 42)
+@store2.employees.create(first_name: "Mike", last_name: "McDonald", hourly_rate: 59)
+@store1.employees.create(first_name: "Adam", last_name: "Peters", hourly_rate: 75)
